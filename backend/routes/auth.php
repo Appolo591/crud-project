16b2c01e1a64fbb9bba26f54eef39ee6935d8147
Your name <Your email>
2026-03-16 17:29:04 +0100

    prod build4

--- backend/routes/auth.php
+++ backend/routes/auth.php
@@ -2,13 +2,14 @@
 require_once __DIR__.'/../config/database.php';
 
 // 1. Headers indispensables pour React
-header("Access-Control-Allow-Origin: https://crud-project-three-alpha.vercel.app/");
-header("Content-Type: application/json");
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: https://crud-project-three-alpha.vercel.app");
 header("Access-Control-Allow-Methods: POST,GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
+header("Content-Type: application/json");
 
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') exit;
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') ;
+    http_response_code(200);
+    exit;
 
 $method = $_SERVER['REQUEST_METHOD'];
 $data = json_decode(file_get_contents('php://input'), true);
